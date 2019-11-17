bbb6c520bd774d7df4999b0436192b78feb11a18
Mario Fonseca <mario_fonseca15@hotmail.com>
2019-11-17 12:57:05 -0700

    Axios Vue

--- Mudanzas/wwwroot/js/site.js
+++ Mudanzas/wwwroot/js/site.js
@@ -7,14 +7,17 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
-        saco:'Nothing'
+        saco:'Nothing',
+        error: true
     },
     methods: {
         holis: function (event) {
             console.log("hola");
             axios
                 .get('https://pokeapi.co/api/v2/pokemon/')
-                .then(response => (this.saco = response))
+                .then(response => (this.saco = response));
+            this.error = false;
         }
     }
 })
+
